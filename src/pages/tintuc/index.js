3b589f8154111be7ea2f64772d7274096e346865
locnt19 <locnt19@gmail.com>
2019-03-22 03:03:13 +0700

    add 07_tintuc_ds

--- src/pages/tintuc/index.js
+++ src/pages/tintuc/index.js
@@ -4,12 +4,42 @@ var swiper = new Swiper('.slide-1 .swiper-container', {
     loop: true,
     speed: 2000,
     autoplay: {
-        delay: 2500,
+        delay: 2000,
         disableOnInteraction: false,
     },
     navigation: {
         nextEl: '.slide-1 .lnr-chevron-right',
         prevEl: '.slide-1 .lnr-chevron-left',
     },
+    breakpoints: {
+        640: {
+            slidesPerView: 2
+        },
+        320: {
+            slidesPerView: 1
+        }
+    }
 });
+
+
 $('.slide-1 [data-fancybox]').fancybox();
+
+
+var swiper = new Swiper('.tintuc-2 .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    speed: 2000,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2
+        },
+        320: {
+            slidesPerView: 1
+        }
+    }
+});
