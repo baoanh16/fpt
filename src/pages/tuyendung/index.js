9904ddbbb5e4ab55a24aacc8a69b7675ff361b6a
locnt19 <locnt19@gmail.com>
2019-03-26 14:52:09 +0700

    add

---
+++ src/pages/tuyendung/index.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.slide-1 .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    speed: 2000,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.slide-1 .lnr-chevron-right',
+        prevEl: '.slide-1 .lnr-chevron-left',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2
+        },
+        576: {
+            slidesPerView: 1
+        }
+    }
+});
+
+
+$('.slide-1 [data-fancybox]').fancybox();
